8641c7c7e9b5789a2ae390839ade6d6944cabe93
Sanchit Mishra <sanchit1417@gmail.com>
2024-09-25 23:15:59 +0530

    user test controller

--- springboot/src/main/java/com/practice/springboot/Controller/UserController.java
+++ springboot/src/main/java/com/practice/springboot/Controller/UserController.java
@@ -1,9 +1,7 @@
 package com.practice.springboot.Controller;
 
 import com.practice.springboot.Entity.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -16,6 +14,29 @@ public class UserController {
         user.setEmail("sanchit@gmail.com");
         user.setId("1");
         user.setName("Sanchit mishra");
+
+        return user;
+    }
+
+    @GetMapping("/get-user-details/{id}")
+    public User getUserById(@PathVariable( value = "id") String userId){
+        User user = new User();
+
+        user.setEmail("sanchit@gmail.com");
+        user.setId(userId);
+        user.setName("Sanchit mishra");
+
+        return user;
+    }
+
+    @GetMapping("/request-user")
+    public User getRequestParamFromUser( @RequestParam(value = "name") String username,
+                                         @RequestParam(value = "email", required = false) String email){
+        User user = new User();
+
+        user.setEmail(email);
+        user.setId("1");
+        user.setName(username);
 
         return user;
     }
